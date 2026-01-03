499ea0785e98aecea4bd3a4067b9330ff0cf93d8
maranzatto <amitael.jau@gmail.com>
2026-01-03 03:34:15 -0300

    feat: inputMoney - ajuste ProductDetail toast

--- src/modules/admin/composables/productDetails/useProductValidation.ts
+++ src/modules/admin/composables/productDetails/useProductValidation.ts
@@ -7,8 +7,28 @@ export const useProductValidation = (form: Ref<ProductForm>, errors: Ref<Product
         errors.value = {};
         let valid = true;
 
+        if (!form.value.name) {
+            errors.value.name = "Nome é obrigatório";
+            valid = false;
+        }
+
+        if (!form.value.barcode) {
+            errors.value.barcode = "Código de barras é obrigatório";
+            valid = false;
+        }
+
         if (!form.value.sku) {
             errors.value.sku = "SKU é obrigatório";
+            valid = false;
+        }
+
+        if (!form.value.salePrice) {
+            errors.value.salePrice = "Preço de venda é obrigatório";
+            valid = false;
+        }
+
+        if (!form.value.stockQuantity) {
+            errors.value.stockQuantity = "Estoque é obrigatório";
             valid = false;
         }
 
